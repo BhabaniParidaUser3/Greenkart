4cbae677c2f6e59de24d060a8e4b36142912942b
bsp8895 <bhabaniparida1995@gmail.com>
2023-12-01 03:12:50 +0530

    12012023-2nd commit

--- greenkart-selenium-java/src/main/java/package1/Base.java
+++ greenkart-selenium-java/src/main/java/package1/Base.java
@@ -1,8 +1,12 @@
 package package1;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
@@ -14,7 +18,31 @@ public class Base {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-		driver.close();
+		
+		//To select particular one product(hardcoded)
+		/*List<WebElement>productName=driver.findElements(By.cssSelector("h4.product-name"));
+		for(int i=0;i<productName.size();i++)
+		{
+			if(productName.get(i).getText().contains("Cucumber"))
+			{
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				break;
+			}
+		}*/
+		
+		String[] itemneed= {"Cauliflower - 1 Kg","Brocolli - 1 Kg","Beetroot - 1 Kg"};
+		List<WebElement>productName=driver.findElements(By.cssSelector("h4.product-name"));
+		for(int i=0;i<productName.size();i++)
+		{
+			String name=productName.get(i).getText();
+			List ItemneededList=Arrays.asList(itemneed);
+			if(ItemneededList.contains(name))
+			{
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+			}
+		}
+		
+		//driver.close();
 
 	}
 
