86cb159f596d1f967a287b6c5b9b4aea9b367461
bsp8895 <bhabaniparida1995@gmail.com>
2023-12-07 00:55:22 +0530

    7thdecember2023-1stcommit

--- greenkart-selenium-java/src/main/java/package1/Base.java
+++ greenkart-selenium-java/src/main/java/package1/Base.java
@@ -13,36 +13,42 @@ public class Base {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","G:\\RahulShetty\\chromedriver-win64\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "G:\\RahulShetty\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		int j=0;
+
+		//Usecase01: To select particular one product(hardcoded)
+		/*
+		 * List<WebElement>productName=driver.findElements(By.cssSelector(
+		 * "h4.product-name")); for(int i=0;i<productName.size();i++) {
+		 * if(productName.get(i).getText().contains("Cucumber")) {
+		 * driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click(
+		 * ); break; } }
+		 */
+        
 		
-		//To select particular one product(hardcoded)
-		/*List<WebElement>productName=driver.findElements(By.cssSelector("h4.product-name"));
-		for(int i=0;i<productName.size();i++)
-		{
-			if(productName.get(i).getText().contains("Cucumber"))
-			{
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				break;
-			}
-		}*/
-		
-		String[] itemneed= {"Cauliflower - 1 Kg","Brocolli - 1 Kg","Beetroot - 1 Kg"};
-		List<WebElement>productName=driver.findElements(By.cssSelector("h4.product-name"));
-		for(int i=0;i<productName.size();i++)
-		{
-			String name=productName.get(i).getText();
-			List ItemneededList=Arrays.asList(itemneed);
-			if(ItemneededList.contains(name))
-			{
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+		//usecase02: To select multiple product
+		String[] itemsneeded = { "Cucumber", "Brocolli", "Beetroot" };
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		for (int i = 0; i < products.size(); i++) {
+			String[] name = products.get(i).getText().split("-");
+			String formattedname = name[0].trim();
+
+			List itemsNeededList = Arrays.asList(itemsneeded);
+			if (itemsNeededList.contains(formattedname)) {
+				j++;
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				if(j==3)
+				{
+					break;
+				}
 			}
 		}
-		
-		//driver.close();
+
+		 driver.close();
 
 	}
 
